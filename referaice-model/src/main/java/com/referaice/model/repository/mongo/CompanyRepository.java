cfbb7375c7ab246c220d5a2af143185bebbb8275
gustavoorsi <gustavoorsi@gmail.com>
2015-03-27 00:37:13 -0300

    spring social + oauth2 security.
    Added overall security with oauth2 and spring social.
    Now you can login/create account using linkedin profile or create a
    referaice.com account (not associated to any social platform)
    Also added some ui mocks.
    Nexts commits will be related to UI mocks more than anything.

--- referaice-model/src/main/java/com/referaice/model/repository/mongo/CompanyRepository.java
+++ referaice-model/src/main/java/com/referaice/model/repository/mongo/CompanyRepository.java
@@ -2,7 +2,7 @@ package com.referaice.model.repository.mongo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.referaice.model.Company;
+import com.referaice.model.entitties.Company;
 
 public interface CompanyRepository extends MongoRepository<Company, String>{
 
