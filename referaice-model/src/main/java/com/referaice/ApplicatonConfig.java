65140e9d18293218b864e9f65ccf86a6a080b196
gustavoorsi <gustavoorsi@gmail.com>
2015-03-27 00:37:25 -0300

    Revert "spring social + oauth2 security."
    This reverts commit cfbb7375c7ab246c220d5a2af143185bebbb8275.

--- referaice-model/src/main/java/com/referaice/ApplicatonConfig.java
+++ referaice-model/src/main/java/com/referaice/ApplicatonConfig.java
@@ -5,28 +5,34 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.referaice.model.Company;
 import com.referaice.model.repository.mongo.CompanyRepository;
-import com.referaice.model.repository.mongo.UserRepository;
 
 @SpringBootApplication
 public class ApplicatonConfig implements CommandLineRunner {
 
 	@Autowired
 	private CompanyRepository companyRepository;
-	
-	@Autowired
-	private UserRepository userRepository;
-
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(ApplicatonConfig.class, args);
-	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
 
-//		this.companyRepository.deleteAll();
-//		this.userRepository.deleteAll();
+		this.companyRepository.deleteAll();
 
+		this.companyRepository.save(new Company("Globant", "a bad company"));
+		this.companyRepository.save(new Company("Google", "a good company"));
+
+		// find and print all companies.
+		this.companyRepository.findAll().forEach(c -> System.out.println(c));
+
+		for (Company c : this.companyRepository.findAll()) {
+			System.out.println(this.companyRepository.findOne(c.getId()));
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ApplicatonConfig.class, args);
 	}
 
 }
